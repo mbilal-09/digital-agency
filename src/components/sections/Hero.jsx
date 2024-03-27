f1d61d65a4ec082afe32b82b1b8f0a570b98bb43
mbilal-09 <bilalkhanhsakir@gmail.com>
2024-03-27 15:20:02 +0500

    hero background

---
+++ src/components/sections/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+      <div class="area">
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Hero
