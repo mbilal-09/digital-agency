6cbbefb16f87db0a49bd143e063e10ce8eb886af
mbilal-09 <bilalkhanhsakir@gmail.com>
2024-03-29 10:49:49 +0500

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,7 @@
-import { Inter as FontSans } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils"
-import Navbar from "@/components/layout/Navbar";
 
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -16,10 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${cn("min-h-screen font-sans antialiased", fontSans.variable)} bg-[--black]`}>
-        <Navbar />
-        {children}
-        </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
