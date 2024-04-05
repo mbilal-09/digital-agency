09b7a2eb0a0731f159ffb474116f1a7fddab420b
mbilal-09 <bilalkhanshakir@gmail>
2024-04-05 16:18:22 +0500

    hero/features/about added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,8 @@ const About = () => {
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 mt-2 dark:text-white">We didn't reinvent the wheel</h2>
                     <p className="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
                     <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
-                    <div className='mt-6'><Button>See More</Button></div>
+                    <div className='mt-6'><Button>See More</Button>
+                    </div>
                 </div>
             </div>
         </section>
