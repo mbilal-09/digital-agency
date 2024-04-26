81be9f2eb1d01297b0955aaa88507a7f198b631f
mbilal-09 <bilalkhanshakir@gmail.com>
2024-04-27 04:02:34 +0500

    initialize

--- src/components/GradientText.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const GradientText = ({ children }) => {
-    return (
-        <span className='bg-gradient-to-r from-secondary to-primary inline-block text-transparent bg-clip-text'>{children}</span>
-    )
-}
-
-export default GradientText
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "node_modules/flowbite-react/lib/esm/**/*.js",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors:{
-        primary: "#2b6af3",
-        secondary: "#5791e9",
-        gray: "#a0a0a0",
-        lightgray: "#e5e7eb",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [],
 };
